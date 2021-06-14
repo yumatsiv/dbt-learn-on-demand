3a50fe04b5e2da96a882913aaa046a3057c0aab0
yurii@creditninja.com <yurii@creditninja.com>
2021-06-14 13:11:34 +0000

    refactored with jinja

---
+++ models/marts/core/int_orders__pivoted.sql
@@ -0,0 +1,20 @@
+{%- set payment_methods = ['credit_card', 'coupon', 'bank_transfer', 'gift_card'] -%}
+
+with payments as (
+    select * from {{ ref('stg_payments') }}
+),
+
+pivoted as (
+    select 
+        order_id,
+        {%- for payment_method in payment_methods %}
+        sum(case when payment_method = '{{ payment_method }}' then amount else 0 end) as {{ payment_method }}_amount
+        {%- if not loop.last -%}
+        ,
+        {%- endif -%}
+        {%- endfor %}
+    from payments
+    group by
+        order_id
+)
+select * from pivoted
